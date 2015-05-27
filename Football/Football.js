7595b100469814577afae5202b52854c6848d274
GitBulk <giacatiensinh@hotmail.com>
2015-05-27 16:48:15 +0700

    add snippets, update UI

--- Football/Football.js
+++ Football/Football.js
@@ -1,23 +1,57 @@
+
+TeamList = new Mongo.Collection("teams");
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    console.log("Hello client");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.teamsranking.helpers({
+        'showTeams': function() {
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+            //  get all and order by score desc, name asc
+            return TeamList.find({}, {sort: {score: -1, name: 1} });
+        },
+        'selectedClass': function() {
+            var teamId = this._id;
+            var selectedTeam = Session.get("selectedTeam");
+            if (teamId == selectedTeam) {
+                return "selected";
+            }
+            return "";
+        },
+        'showSelectedTeam': function() {
+            var selectedTeam = Session.get("selectedTeam");
+            return TeamList.findOne(selectedTeam);
+        }
+
+    }); // end of helpers
+
+
+    Template.teamsranking.events({
+        'click li.team': function() {
+            var teamId = this._id;
+            Session.set("selectedTeam", teamId);
+            //var selectedTeam = Session.get("selectedTeam");
+            //console.log("You selected: " + selectedTeam);
+        },
+        'click .increment': function() {
+            var selectedTeam = Session.get("selectedTeam");
+            //console.log("You selected: " + selectedTeam);
+            TeamList.update(selectedTeam, {$inc: {score: 5} });
+        },
+        'click .decrement': function() {
+            var selectedTeam = Session.get("selectedTeam");
+            //console.log("You selected: " + selectedTeam);
+            TeamList.update(selectedTeam, {$inc: {score: -5} });
+        }
+    });
+
+    Template.addTeamForm.events({
+        'submit form': function() {
+            console.log("Form submitted");
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    console.log("Hello server");
 }
+
